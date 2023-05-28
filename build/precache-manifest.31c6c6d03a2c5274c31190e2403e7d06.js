2957d6549b72c0d5cfdc9f9afe1b1fe257262c1d
Shetteemah <shettimaali58@yahoo.com>
2023-05-28 19:46:05 +0200

    update

---
+++ build/precache-manifest.31c6c6d03a2c5274c31190e2403e7d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38a7b8d3886225bc8171",
+    "url": "/static/css/main.bc000662.chunk.css"
+  },
+  {
+    "revision": "38a7b8d3886225bc8171",
+    "url": "/static/js/main.38a7b8d3.chunk.js"
+  },
+  {
+    "revision": "c85c368c23eddb15a8e9",
+    "url": "/static/js/1.c85c368c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93070ed09e4b907c3c3385ad1278ef98",
+    "url": "/static/media/shettima logo 150px.93070ed0.png"
+  },
+  {
+    "revision": "a4ed8f87c2c0f8cce0170e2fb235c1de",
+    "url": "/index.html"
+  }
+];
